9c1e0980e12bd80cfd54ac1e1b0a25eb600060fb
vineethn7 <mailtovineethram@gmail.com>
2020-03-10 00:08:04 +0530

    user register is now working

--- PortalSleuth/settings.py
+++ PortalSleuth/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'userRegister.apps.UserregisterConfig',
     'PortalSleuthMain.apps.PortalsleuthmainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'PortalSleuth-home'
+LOGIN_URL = 'login'
--- PortalSleuthMain/urls.py
+++ PortalSleuthMain/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='PortalSleuth-home'),
+    path('home/',views.home, name='PortalSleuth-home'),
 ]
